2bb1cb64b2a061dda6ea56995a24f46c9750d3c0
kamal <tripathikamalnayan3@gmail.com>
2025-12-08 17:59:10 +0530

    Added the last pattern question

--- patterns/pattern22.js
+++ patterns/pattern22.js
@@ -2,19 +2,22 @@
 
 const n=4;
 
-for(let i=0; i<n; i++){
+for(let i=0; i<2*n-1; i++){
     for(let j=0; j<2*n-1; j++){
-        if(i>0 && i<n-1){
-            if(j===0 || j===2*n-2){
-                process.stdout.write(`${n}`)
-            }else{
-                process.stdout.write(`${n-i}`)
-            }
-        }else{
-            process.stdout.write(`${n}`)
-        }
+        const top = i, left = j;
+        const right = 2*n-2-i, bottom = 2*n-2-j;
+        const val = n-Math.min(top, left, right, bottom)
+        process.stdout.write(`${val}`)
     }
     console.log()
 }
 
 //expected output
+
+// 4444444
+// 4333334
+// 4322234
+// 4321234
+// 4322234
+// 4333334
+// 4444444
