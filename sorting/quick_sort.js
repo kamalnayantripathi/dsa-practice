b61333d6147458c4f11e2d023f637c206f51d4f5
kamal <tripathikamalnayan3@gmail.com>
2025-12-19 01:04:20 +0530

    Solve: Quick sort algorithm

--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -1,16 +1,37 @@
-function quickSortRecursion(arr){
-    if(arr.length===1) return arr;
-    const piv=arr[0];
-    for(let i=1;i<arr.length;i++){ 
-        if(piv>arr[i]){
-            const temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+
+function quickSort(arr,low,high){
+    console.log("start",arr,low,high)
+    if(low>=high) return arr;
+    const piv=arr[low];
+    let i=low,j=high;
+    while(i<j){
+        console.log("while",i,j,arr[i],piv,arr[j])
+        while(i<=high-1 && arr[i]<=piv){
+            i++;
+            console.log("i",i)
         }
+        while(j>=low+1 && arr[j]>piv){
+            j--;
+            console.log("j",j)
+        }
+        if(i<j) console.log("swapped",swap(arr,i,j))
     }
-    quickSortRecursion()
+    swap(arr,j,low) 
+    console.log("piv",piv, j, arr)
+    quickSort(arr,low,j-1)
+    quickSort(arr,j+1,high)
+    return arr;
 }
 
-const array = [13,46,24,52,20,9];
-// const array = [5,4,3,2,1];
-console.log(quickSortRecursion(array), "<-- Sorted in Ascending order");
+// const array = [13,46,24,52,20,9];
+const array = [5,4,3,2,1];
+console.log(quickSort(array,0,array.length-1), "<-- Sorted in Ascending order");
+
+// Swaps the values in the array
+function swap(array,first,second){
+    console.log("In",array,"swap",first,second)
+    const temp=array[first];
+    array[first]=array[second];
+    array[second]=temp;
+    return array;
+}
