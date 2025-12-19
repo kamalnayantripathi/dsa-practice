20002910f1d832d0b2b233477a8240920189afc6
kamal <tripathikamalnayan3@gmail.com>
2025-12-19 17:28:01 +0530

    Solve: Add recursive approaches to bubble and insertion sorts

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,3 +1,4 @@
+// Compare every element to it's next one and swap if needed. Keep repeating for till sorted.
 function bubbleSortLoop(arr){
     for(let i=0;i<arr.length;i++){ 
         for(let j=0;j<arr.length-1-i;j++){
@@ -10,6 +11,20 @@ function bubbleSortLoop(arr){
     }
     return arr;
 }
-// const array = [13,46,24,52,20,9];
-const array = [5,4,3,2,1];
-console.log(bubbleSortLoop(array), "<-- Sorted in Ascending order");
+
+function bubbleSortRec(arr,i){
+    console.log(i,arr)
+    if(i>=arr.length) return arr;
+    for(let j=0;j<arr.length-1-i;j++){
+        if(arr[j]>arr[j+1]){
+            const temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+    return bubbleSortRec(arr,++i);
+}
+const array = [13,46,24,52,20,9];
+// const array = [5,4,3,2,1];
+// console.log(bubbleSortLoop(array), "<-- Sorted in Ascending order(Loop)");
+console.log(bubbleSortRec(array,0), "<-- Sorted in Ascending order(Recursion)");
--- sorting/insertion_sort.js
+++ sorting/insertion_sort.js
@@ -14,6 +14,23 @@ function insertionSortLoop(arr){
     }
     return arr;
 }
-const array = [13,46,24,52,20,9];
-// const array = [5,4,3,2,1];
-console.log(insertionSortLoop(array), "<-- Sorted in Ascending order");
+
+function insertionSortRec(arr, i){
+    if(i>=arr.length) return;
+    let j=i+1;
+    console.log(j,i,arr)
+    while(j<arr.length){
+        if(arr[i]>arr[j]){
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        j++;
+    }
+    return insertionSortRec(arr,++i);
+}
+
+// const array = [13,46,24,52,20,9];
+const array = [5,4,3,2,1];
+// console.log(insertionSortLoop(array), "<-- Sorted in Ascending order(Loop)");
+console.log(insertionSortLoop(array, 0), "<-- Sorted in Ascending order(Recursion)");
