f83825145eae840d3911fb65101c9ed6fbb8a610
kamal <tripathikamalnayan3@gmail.com>
2025-12-23 03:25:53 +0530

    Solve: majority element better approach using hashmap

--- arrays/medium/majority_element.js
+++ arrays/medium/majority_element.js
@@ -6,9 +6,18 @@ var majorityElement = function(nums) {
     // sort the array and return the middle element
     // nums.sort((a,b) => a-b)
     // return nums[Math.floor(nums.length/2)]
+
+    // Better approach, Time complexity(TC): O(n), Space complexity(SC): O(n)
+    const freq = new Map();
+    const required = Math.floor(nums.length/2)
+    for(let i=0; i<nums.length; i++){
+        freq.set(nums[i], (freq.get(nums[i]) || 0)+1)
+        console.log(freq)
+        if(freq.get(nums[i])>required) return nums[i];
+    }
 };
  
-const nums = [3,2,3]
-// const nums = [2,2,1,1,1,2,2]
+// const nums = [3,2,3]
+const nums = [2,2,1,1,1,2,2]
 
 console.log(majorityElement(nums))
