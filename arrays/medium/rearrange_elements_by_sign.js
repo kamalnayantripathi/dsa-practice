9df8de760726caf8942fdf77f06837160d7f842c
kamal <tripathikamalnayan3@gmail.com>
2025-12-24 17:17:01 +0530

    Solve: Rearrange array with constant space complexity

--- arrays/medium/rearrange_elements_by_sign.js
+++ arrays/medium/rearrange_elements_by_sign.js
@@ -7,19 +7,45 @@
 var rearrangeArray = function(nums) {
     // Brute force: traverse through nums, check for their sign and keep (+,-) pairs in a new array.
     // Time complexity(TC): O(n), Space Complexity(SC): O(n)
-    let arr=[],j=0,k=1;
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]>0){
-            arr[j]=nums[i]
-            j+=2;
-        }else{
-            arr[k]=nums[i]
-            k+=2;
-        }
-    }
-    return arr;
-};
+    // let arr=[],j=0,k=1;
+    // for(let i=0; i<nums.length; i++){
+    //     if(nums[i]>0){
+    //         arr[j]=nums[i]
+    //         j+=2;
+    //     }else{
+    //         arr[k]=nums[i]
+    //         k+=2;
+    //     }
+    // }
+    // return arr;
 
+    // Alternative solution: Time complexity: ~O(n*n), Space complexity: O(1)
+    // Will result into TLE on Leetcode or for big values of nums.length like 10^5;
+    // let c=0,i=1;
+    // while(c<nums.length){
+    //     console.log(c,i)
+    //     if((c%2===0 && nums[c]>0) || (c%2!==0 && nums[c]<0)) i=++c;
+    //     else if(c%2===0 && nums[c]<0){
+    //         if(nums[i]>0) swap(nums,i,c++)
+    //         else swap(nums,i,c)
+    //     }else if(c%2!==0 && nums[c]>0){
+    //         if(nums[i]<0) swap(nums,i,c++)
+    //         else swap(nums,i,c)
+    //     }
+    //     i++;
+    // }
+};
+// const nums = [-2,-3,-4,1,2,3]
 // const nums = [3,1,-2,-5,2,-4]
-const nums = [-1,1]
-console.log(rearrangeArray(nums))
+// const nums = [-1,1]
+// const nums = [28,-41,22,-8,-37,46,35,-9,18,-6,19,-26,-37,-10,-9,15,14,31]
+// const nums = [1,-2,-3,-4,5,6]
+// rearrangeArray(nums)
+console.log(nums.length)
+
+function swap(nums, i, j){
+    const temp = nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+    console.log(i,j,"swapped")
+}
