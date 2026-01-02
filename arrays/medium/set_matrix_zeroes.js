5282032f2ceb01e9d19270ecb41259556ddcc6aa
kamal <tripathikamalnayan3@gmail.com>
2026-01-02 11:59:50 +0530

    Solve: set matrix zeroes alternate solution

--- arrays/medium/set_matrix_zeroes.js
+++ arrays/medium/set_matrix_zeroes.js
@@ -3,27 +3,48 @@
 // You must do it in place.
 
 var setZeroes = function(matrix) {
-    // Brute force: Time complexity(TC): O(2m*n), Space complexity: ~O(n/5+m/5)
-    const rows=[],cols=[],m=matrix.length;n=matrix[0].length;
+    // Brute force: Time complexity(TC): O(2m*n) assuming hashing takes O(1), Space complexity: ~O(m+n)
+    // const rows=new Set(),cols=new Set(), m=matrix.length;n=matrix[0].length;
+    // for(let i=0; i<m; i++){
+    //     for(let j=0; j<n; j++){
+    //         if(matrix[i][j]===0){
+    //             rows.add(i)
+    //             cols.add(j)
+    //         }
+    //     }
+    // }
+    // console.log(rows, cols)
+    // for(let i=0; i<m; i++){
+    //     for(let j=0; j<n; j++){
+    //         if(rows.has(i) || cols.has(j)) matrix[i][j]=0;
+    //     }
+    // }
+
+    // Alternate solution: Time complexity(TC): O((m*n)*(m+n)+(m*n)), Space complexity: ~O(1)
+    const m=matrix.length;n=matrix[0].length;
     for(let i=0; i<m; i++){
         for(let j=0; j<n; j++){
             if(matrix[i][j]===0){
-                rows.push(i)
-                cols.push(j)
+                for(let k=0; k<m; k++){ 
+                    console.log(k,j,matrix[k][j])
+                    matrix[k][j]=matrix[k][j]!==0 ? null : 0;
+                }
+                for(let k=0; k<n; k++){ 
+                    matrix[i][k]=matrix[i][k]!==0 ? null : 0;
+                }
             }
         }
     }
-    console.log(rows, cols)
+    console.log(matrix)
     for(let i=0; i<m; i++){
         for(let j=0; j<n; j++){
-            if(rows.includes(i) || cols.includes(j)) matrix[i][j]=0;
+            if(!matrix[i][j]) matrix[i][j]=0;
         }
     }
-    return matrix;
 };
 
-// const matrix = [[1,1,1],[1,0,1],[1,1,1]] // Output: [[1,0,1],[0,0,0],[1,0,1]]
+const matrix = [[1,1,1],[1,0,1],[1,1,1]] // Output: [[1,0,1],[0,0,0],[1,0,1]]
 // const matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] // Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
-matrix = [[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]]
+// matrix = [[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]]
 
-console.log(setZeroes(matrix))
+console.log(setZeroes(matrix), matrix)
