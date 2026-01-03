a40d278651a666228acaa13871d45d6cfec52df3
kamal <tripathikamalnayan3@gmail.com>
2026-01-03 20:34:26 +0530

    Solve: Alternate approach majority element 2

--- arrays/hard/majority_element2.js
+++ arrays/hard/majority_element2.js
@@ -3,20 +3,32 @@
 
 var majorityElement = function(nums) {
     // Brute force: Time complexity(TC): ~O(n*n), Space complexity(SC): ~O(n)
-    const ans=[]
+    // const ans=[]
+    // for(let i=0; i<nums.length; i++){
+    //     count=1;
+    //     for(let j=i+1; j<nums.length; j++){
+    //         if(nums[i]===nums[j]) count++
+    //     }
+    //     if(count>Math.floor(nums.length/3)) {
+    //         if(!ans.includes(nums[i])) ans.push(nums[i])
+    //     }
+    // }
+    // return ans
+
+    // Using maps
+    const ans=[], map = new Map();
     for(let i=0; i<nums.length; i++){
-        count=1;
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]===nums[j]) count++
-        }
-        if(count>Math.floor(nums.length/3)) {
-            if(!ans.includes(nums[i])) ans.push(nums[i])
-        }
+        map.set(nums[i], (map.get(nums[i]) || 0)+1)
     }
+    for(const [key, val] of map){
+        if(val>Math.floor(nums.length/3)) ans.push(key)
+    }
+    console.log(map)
     return ans
 };
 
 // const nums = [3,2,3] // Output: [3]
 // const nums = [1] // Output: [1]
-const nums = [1,2] // Output: [1,2]
+// const nums = [1,2] // Output: [1,2]
+const nums = [2,2,2,2,2] // Output: [2]
 console.log(majorityElement(nums))
