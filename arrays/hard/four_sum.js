e20fe31ad227d605f49d35a2c475a69ef1396108
kamal <tripathikamalnayan3@gmail.com>
2026-01-12 17:30:48 +0530

    Solve: 4Sum optimal solution

--- arrays/hard/four_sum.js
+++ arrays/hard/four_sum.js
@@ -7,28 +7,50 @@
 var fourSum = function(nums, target) {
     // Brute approach: TC: O(n^3), SC: ~O(2*n) or O(n+ans.length)
     console.log(nums,target)
-    const map = new Map()
-    for(let i=0; i<nums.length; i++){
+    // const map = new Map()
+    // for(let i=0; i<nums.length; i++){
+    //     map.set(nums[i],i)
+    // }
+    // console.log(map)
+    // const set = new Set()
+    // const ans=[]
+    // for(let i=0; i<nums.length; i++){
+    //     for(let j=i+1; j<nums.length; j++){
+    //         for(let k=j+1; k<nums.length; k++){
+    //             const val=target-(nums[i]+nums[j]+nums[k])
+    //             const temp = map.get(val)
+    //             if((i===temp || j===temp || k===temp)) continue;
+    //             if(temp!==undefined){
+    //                 const sorted = [nums[i],nums[j],nums[k],val].sort((a,b)=>a-b)
+    //                 if(set.has(sorted.join(""))) continue;
+    //                 ans.push([nums[i],nums[j],nums[k],val])
+    //                 set.add(sorted.join(""))
+    //                 // console.log(val,ans)
+    //             }
+    //         }
+    //     }
+    // }
+    // return ans
+
+    // Optimal solution: TC: O(n^3), SC: O(n)
+    nums.sort((a,b) => a-b)
+    const map = new Map(), ans=[]
+    for(let i=0; i<nums.length; i++){ 
         map.set(nums[i],i)
     }
     console.log(map)
-    const set = new Set()
-    const ans=[]
     for(let i=0; i<nums.length; i++){
         for(let j=i+1; j<nums.length; j++){
             for(let k=j+1; k<nums.length; k++){
                 const val=target-(nums[i]+nums[j]+nums[k])
-                const temp = map.get(val)
-                if((i===temp || j===temp || k===temp)) continue;
-                if(temp!==undefined){
-                    const sorted = [nums[i],nums[j],nums[k],val].sort((a,b)=>a-b)
-                    if(set.has(sorted.join(""))) continue;
-                    ans.push([nums[i],nums[j],nums[k],val])
-                    set.add(sorted.join(""))
-                    // console.log(val,ans)
-                }
+                const l = map.get(val)
+                if(l===undefined || k>=l) continue;
+                ans.push([nums[i],nums[j],nums[k],val])
+                while(nums[k+1]===nums[k]) k++
             }
+            while(nums[j+1]===nums[j]) j++
         }
+        while(nums[i+1]===nums[i]) i++
     }
     return ans
 };
@@ -42,11 +64,12 @@ var fourSum = function(nums, target) {
 // nums = [-5, -4, -3, -2, -1], target = -10 // Expected: [[-4, -3, -2, -1]]
 // nums = [1, 2, 3, 4, 5], target = 10 // Expected: [[1, 2, 3, 4]]
 // nums = [1, 2, 3], target = 6// Expected: []
+// nums = [1, 0, -1, 0, -2, 2, 2], target = 2 // Expected: [  [-2, 0, 2, 2], [-1, 0, 1, 2] ]
+nums = [-3, -1, 0, 2, 4, 5], target = 2 // Expected: [[-3, -1, 2, 4]]
 // nums = [1000000000, 1000000000, 1000000000, 1000000000] ,target = 4000000000
 // Expected: [[1000000000, 1000000000, 1000000000, 1000000000]]
-nums = [-1000000000, -1000000000, 0, 0, 1000000000, 1000000000] ,target = 0
+
+// nums = [-1000000000, -1000000000, 0, 0, 1000000000, 1000000000] ,target = 0
 // Expected: [[-1000000000, -1000000000, 1000000000, 1000000000],[-1000000000, 0, 0, 1000000000]]
-// nums = [1, 0, -1, 0, -2, 2, 2], target = 2 // Expected: [  [-2, 0, 2, 2], [-1, 0, 1, 2] ]
-// nums = [-3, -1, 0, 2, 4, 5], target = 2 // Expected: [[-3, -1, 2, 4]]
 
 console.log(fourSum(nums,target))
